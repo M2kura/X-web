f1ad8827f446551c44a01795a86097ed4bc470d3
George Makura <teterheo@cvut.cz>
2024-11-13 02:46:25 +0000

    Add header component

---
+++ views/components/header.php
@@ -0,0 +1,18 @@
+<header id="header">
+    <div class="logo-div">
+        <div class="logo-crop">
+            <img src="./media/web/logo_white.svg" alt="Logo" class="logo-img">
+        </div>
+    </div>
+    <div class="profile-link">
+        <a>Profile</a>
+    </div>
+    <div class="create-post">
+        <a href="">Create Post</a>
+    </div>
+    <div>
+        <form action="./php_scripts/logout_script.php" method="POST">
+            <button type="submit">Log out</button>
+        </form>
+    </div>
+</header>
--- views/home_page.php
+++ views/home_page.php
@@ -1,8 +1,6 @@
 <?php 
-
 require "./php_scripts/check_session.php";
 isSessionDown();
-
 ?>
 
 <!DOCTYPE html>
@@ -10,12 +8,10 @@ isSessionDown();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/home_page.css">
     <title>Twixter: Home Page</title>
 </head>
 <body>
-    <p>Home Page</p>
-    <form action="./php_scripts/logout_script.php" method="POST">
-        <button type="submit">Log out</button>
-    </form>
+    <?php require "components/header.php" ?>
 </body>
 </html>
