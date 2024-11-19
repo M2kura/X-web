8285e2005e7425adf792329496d5d86f1ced6d82
George Makura <teterheo@cvut.cz>
2024-11-19 09:47:07 +0000

    Change logo sizing, add admin page button

--- views/components/header.php
+++ views/components/header.php
@@ -1,15 +1,18 @@
 <header id="header">
     <div class="logo-div">
-        <div class="logo-crop">
             <img src="./media/web/logo_white.svg" alt="Logo" class="logo-img">
-        </div>
     </div>
     <div class="profile-link">
-        <a>Profile</a>
+        <a href="/~teterheo/profile">Profile</a>
     </div>
     <div class="create-post">
         <a href="">Create Post</a>
     </div>
+    <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
+        <div class="admin-section">
+            <a href="admin_dashboard.php">Admin Dashboard</a>
+        </div>
+    <?php endif; ?>
     <div>
         <form action="./php_scripts/logout_script.php" method="POST">
             <button type="submit">Log out</button>
